3ed0c349ecab2e3fe60041d838ce5c1a95bc69f3
DennyDaVjncj <daVjncjCode@gmail.com>
2025-06-07 16:30:52 -0400

    configured logic to capture user selections via dropdown, failed, to update dom

--- cns/impulse.js
+++ cns/impulse.js
@@ -8,15 +8,13 @@ var corpus = {
   'Y': 'L','Z': 'M'
 };
 
-// const btn = Document.getElementById("rot12");
-// if (btn) {
-// btn.addEventListener("click",()=> {
-//   const p = Document.getElementById("rot13");
-//   if (p) {
-//     p.innerText = injctn13(p.innerText)//I want this to result in the text being updated to a shifted value, according to the amount selected by the user.
-//   }
-// });
-// }
+const btn = document.getElementById("rot12").addEventListener("click",()=>{
+  const drpDwn=document.getElementById("picker");
+  const rShift=parseInt(drpDwn.value,10);
+  const uiCntnt=document.getElementById("rot13");
+
+  if(uiCntnt)uiCntnt.innerText=injctn13(uiCntnt.innerText,rShift);
+});
 
 const injctn13=str=> {
   let codeDataStructure = str.split(" ");
@@ -32,7 +30,7 @@ const injctn13=str=> {
   console.log(str)
   return str;
 }
-injctn13("le da Vjncj code");//my hunch is that this value should be linked to the values selected by user, from dropdown, and then the text on screen, should be shifted according to that value.
+injctn13(btn);//my hunch is that this value should be linked to the values selected by user, from dropdown, and then the text on screen, should be shifted according to that value.
 
 /**
  * collect values from dropdown, and then shift the text on screen, according to that value. According to priciples from the cesar cipher, the text should be shifted by the value selected by user.
